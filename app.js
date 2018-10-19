d57716cf65634637f8072f273b7a10f90e8da11f
santhosh <singumahanthisanthosh@gmail.com>
2018-10-19 16:49:37 +0530

    cors modified

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(bodyParser.json());
 app.use((req,res,next)=>{
     //* will give access to any origin
  res.header('Access-Control-Allow-Origin','*');
- res.header('Access-Control-Allow-Origin','Origin,X-Requested-With,Content-Type,Accept,Accept');
+ res.header('Access-Control-Allow-Origin','Origin,X-Requested-With,Content-Type,Accept');
 //  if(req.method === 'OPTIONS'){
 //      res.header('Access-Control-Allow-Methods','PUT, POST, PATCH, DELETE, GET');
 //      return res.status(200).json({});
