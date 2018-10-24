5c5d329919c4dcc9df5b2d0466a12805ff6de0b7
santhosh <singumahanthisanthosh@gmail.com>
2018-10-25 00:18:45 +0530

    order schema updated

--- models/order.js
+++ models/order.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const orderSchema = mongoose.Schema({
     _id : mongoose.Schema.Types.ObjectId,
- product : { type :mongoose.Schema.Types.ObjectId, ref :'Product', required : true},
+ food : { type :mongoose.Schema.Types.ObjectId, ref :'Food', required : true},
  quantity : {type :Number, default : 0}
 
 });
